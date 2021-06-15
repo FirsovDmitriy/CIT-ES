e939b4f0d2267f2545f3bf00c021d2c38717e27e
FirsovDmitriy <firsov.dmitriy666@gmail.com>
2021-06-16 01:27:35 +0400

    updated files

--- src/js/import/components.js
+++ src/js/import/components.js
@@ -1,2 +1,25 @@
-// require ("swiper/swiper-bundle.css");
+require ("swiper/swiper-bundle.css");
 
+import Swiper from 'swiper';
+
+
+// core version + navigation, pagination modules:
+import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+
+// configure Swiper to use modules
+SwiperCore.use([Navigation, Pagination]);
+
+
+new Swiper('.swiper-container', {
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
